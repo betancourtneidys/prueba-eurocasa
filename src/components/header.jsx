382ef7726fc9e49869b68585dbc0eae55746403a
Neidys Betancourt <betancourtneidys@gmail.com>
2022-02-04 23:55:54 -0300

    Add header

---
+++ src/components/header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../styles/header.css';
+import logo from '../assets/LOGO.png'
+
+const Header = () => {
+  return (
+    <nav className='header'>
+      <img className='logo' src={logo}  alt="Logo Eurocasa" />
+      <ul className="navbar">
+        <li>
+          <a href="/">INICIO</a>
+        </li>
+        <li>
+          <a href="/">PRODUCTOS</a>
+        </li>
+        <li>
+          <a href="/">CONTACTO</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
