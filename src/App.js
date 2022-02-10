7001c8866c8cd4137868c78bf630b4a8a7311966
Neidys Betancourt <betancourtneidys@gmail.com>
2022-02-10 18:34:57 -0300

    Add Main section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Header from './components/header';
+import Home from './components/home';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Home />
+    </>
+    
   );
 }
 
---
+++ src/components/home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/home.css';
+
+const Home = () => {
+  return (
+    <section>
+      <div className='home'>
+        <h2>MARCA TU</h2>
+        <h1>ESTILO PROPIO</h1>
+      </div>
+      <div>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
